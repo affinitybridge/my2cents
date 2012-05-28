03d6c1a15873dcb48376a2d5286f630b8ddc63e0
Shawn Price <shawnp@gmail.com>
2012-05-28 14:36:37 -0700

    json route for representatives by lat/lon

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require('express'),
     //TwilioClient = require('twilio').Client,
-    Capability = require('twilio').Capability;
+    Capability = require('twilio').Capability,
+    represent = require('represent');
 
 var app = express.createServer();
 
@@ -24,6 +25,15 @@ app.get('/', function (req, res) {
   });
 });
 
+app.get('/representatives/:lat/:lon', function (req, res) {
+  //if (req.xhr) {
+  represent.representativesLatLon(req.params.lat, req.params.lon, function (reps) {
+    var processedReps = processReps(reps);
+    res.json(processedReps);
+  });
+  //}
+});
+
 app.get('/twiml', function (req, res) {
   var number = req.param('PhoneNumber');
   res.contentType('text/xml');
@@ -33,6 +43,30 @@ app.get('/twiml', function (req, res) {
     callerID: process.env.TWILIO_CALLER_ID
   });
 });
+
+// @TODO: This might need to become async
+function processReps(reps) {
+  var output = [];
+  for (i in reps.objects) {
+    var rep = reps.objects[i];
+    var phone = '';
+    for (o in rep.offices) {
+      if (rep.offices[o].tel && (rep.offices[o].type.match(/Constituency/i))) {
+        var phone = rep.offices[o].tel;
+        break;
+      }
+    }
+    output.push(
+      {
+        name: rep.name,
+        title: rep.district_name + ' ' + rep.elected_office + ' at ' + rep.representative_set_name,
+        phone: phone,
+        email: rep.email
+      }
+    );
+  }
+  return output;
+}
 
 var port = process.env.PORT || 3000;
 app.listen(port, function() {
