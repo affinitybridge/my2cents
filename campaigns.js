f7a065824f81be6f9bf42fd7159758619e5e87ea
Raphael Huefner <raphael@affinitybridge.com>
2012-05-30 15:54:07 -0700

    put campaign id into campaign object

--- campaigns.js
+++ campaigns.js
@@ -1,6 +1,6 @@
 var campaigns = {
-  '1': {title:'Our awesome Campaign', script:'We are so awesome! Go and make this a law!'},
-  '2': {title:'Our other awesome Campaign', script:'We are still so awesome! Go already and make this a law! GO! GO! GO!'}
+  '1': {id:'1', title:'Our awesome Campaign', script:'We are so awesome! Go and make this a law!'},
+  '2': {id:'2', title:'Our other awesome Campaign', script:'We are still so awesome! Go already and make this a law! GO! GO! GO!'}
 }
 
 module.exports.findAll = function (callback) {
