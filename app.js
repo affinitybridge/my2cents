4a0db550a88b95cb6b14fec6305970cf4223efb5
Shawn Price <shawnp@gmail.com>
2012-08-09 15:51:53 -0700

    Only display representatives with phone numbers

--- app.js
+++ app.js
@@ -331,20 +331,21 @@
       for (var office_index = 0; office_index < rep.offices.length; office_index++) {
         if (rep.offices[office_index].tel && rep.offices[office_index].type.match(/Constituency/i)) {
           phone = rep.offices[office_index].tel;
+          // Only include representatives with phone numbers.
+          output.push(
+            {
+              id: index,
+              name: rep.name,
+              title: rep.district_name + ' ' + rep.elected_office + ' at ' + rep.representative_set_name,
+              phone: phone,
+              phoneHash: miniHash(phone, process.env.SALT),
+              email: rep.email,
+              photo: rep.photo_url
+            }
+          );
           break;
         }
       }
-      output.push(
-        {
-          id: index,
-          name: rep.name,
-          title: rep.district_name + ' ' + rep.elected_office + ' at ' + rep.representative_set_name,
-          phone: phone,
-          phoneHash: miniHash(phone, process.env.SALT),
-          email: rep.email,
-          photo: rep.photo_url
-        }
-      );
     }
     return output;
   }
