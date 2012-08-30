a9b24248cd2cdaf8f0a5df3870091a783c933de9
Shawn Price <shawnp@gmail.com>
2012-08-30 14:54:30 -0700

    Remove IIFE

--- app.js
+++ app.js
@@ -1,106 +1,105 @@
-  // **My2Cents** is a campaign widget for calling elected
-  // representatives. Source is available on
-  // [GitHub](https://github.com/affinitybridge/my2cents).
+// **My2Cents** is a campaign widget for calling elected
+// representatives. Source is available on
+// [GitHub](https://github.com/affinitybridge/my2cents).
 
-  // My2Cents is built on [Express](http://expressjs.com/). It uses the
-  // [Twilio API](http://twilio.com) to make phone calls and the
-  // [Open North Represent API](http://represent.opennorth.ca/) to receive
-  // representative information. [Jade](http://jade-lang.com/) is used for templating and
-  // [Stylus](http://learnboost.github.com/stylus/) helps with CSS.
-(function() {
-  var express = require('express'),
-      passport = require('passport'),
-      stylus = require('stylus'),
-      config = require('./config');
+// My2Cents is built on [Express](http://expressjs.com/). It uses the
+// [Twilio API](http://twilio.com) to make phone calls and the
+// [Open North Represent API](http://represent.opennorth.ca/) to receive
+// representative information. [Jade](http://jade-lang.com/) is used for templating and
+// [Stylus](http://learnboost.github.com/stylus/) helps with CSS.
 
-  // Create the Express server.
-  var app = module.exports = express.createServer();
+var express = require('express'),
+    passport = require('passport'),
+    stylus = require('stylus'),
+    config = require('./config');
 
-  // Stylus compile function. [[docs](http://learnboost.github.com/stylus/docs/middleware.html)]
-  function compile(str, path) {
-    return stylus(str)
-      .set('filename', path)
-      // Comment the processed CSS when not on production
-      .set('linenos', process.env.NODE_ENV !== 'production')
-      // Compress the processed CSS
-      .set('compress', true);
+// Create the Express server.
+var app = module.exports = express.createServer();
+
+// Stylus compile function. [[docs](http://learnboost.github.com/stylus/docs/middleware.html)]
+function compile(str, path) {
+  return stylus(str)
+    .set('filename', path)
+    // Comment the processed CSS when not on production
+    .set('linenos', process.env.NODE_ENV !== 'production')
+    // Compress the processed CSS
+    .set('compress', true);
+}
+
+// Configure Express application.
+// Express uses [Connect](http://www.senchalabs.org/connect/) middleware.
+
+var oneMonth = 1000 * 60 * 60 * 24 * 30;
+
+app.configure(function() {
+  app.use(stylus.middleware({
+    src: __dirname + '/styles',
+    dest: __dirname + '/public',
+    compile: compile
+  }));
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  app.set('port', process.env.PORT || 5000);
+  app.use(express.logger());
+  app.use(express.bodyParser());
+  app.use(express.compress());
+  app.use(express.static(__dirname + '/public', { maxAge: oneMonth }));
+  app.use(express.cookieParser());
+  app.use(express.session({secret: process.env.SESSION_SECRET || config.session_secret}));
+  app.use(passport.initialize());
+  app.use(passport.session());
+  app.use(express.favicon('public/favicon.ico', { maxAge: oneMonth }));
+});
+
+app.configure('development', function(){
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+});
+
+app.configure('test', function(){
+  app.set('port', 5001);
+});
+
+app.configure('production', function(){
+  app.use(express.errorHandler());
+});
+
+// Dynamic view helpers
+app.dynamicHelpers({
+  flash: function (req, res) {
+    return req.flash();
   }
+});
 
-  // Configure Express application.
-  // Express uses [Connect](http://www.senchalabs.org/connect/) middleware.
-
-  var oneMonth = 1000 * 60 * 60 * 24 * 30;
-
-  app.configure(function() {
-    app.use(stylus.middleware({
-      src: __dirname + '/styles',
-      dest: __dirname + '/public',
-      compile: compile
-    }));
-    app.set('views', __dirname + '/views');
-    app.set('view engine', 'jade');
-    app.set('port', process.env.PORT || 5000);
-    app.use(express.logger());
-    app.use(express.bodyParser());
-    app.use(express.compress());
-    app.use(express.static(__dirname + '/public', { maxAge: oneMonth }));
-    app.use(express.cookieParser());
-    app.use(express.session({secret: process.env.SESSION_SECRET || config.session_secret}));
-    app.use(passport.initialize());
-    app.use(passport.session());
-    app.use(express.favicon('public/favicon.ico', { maxAge: oneMonth }));
-  });
-
-  app.configure('development', function(){
-    app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
-  });
-
-  app.configure('test', function(){
-    app.set('port', 5001);
-  });
-
-  app.configure('production', function(){
-    app.use(express.errorHandler());
-  });
-
-  // Dynamic view helpers
-  app.dynamicHelpers({
-    flash: function (req, res) {
-      return req.flash();
-    }
-  });
-
-  // User authentication middleware
-  function requireAuth(req, res, next) {
-    if (req.session.user) {
-      next();
-    }
-    else {
-      req.flash('error', 'Please login to access this page!');
-      res.redirect('/login');
-    }
+// User authentication middleware
+function requireAuth(req, res, next) {
+  if (req.session.user) {
+    next();
   }
+  else {
+    req.flash('error', 'Please login to access this page!');
+    res.redirect('/login');
+  }
+}
 
-  // ### Routes
+// ### Routes
 
-  // Homepage
-  require('./routes/index')(app);
-  // User authentication
-  require('./routes/login')(app);
-  // User creation
-  require('./routes/user')(app);
-  // User dashboard
-  require('./routes/dashboard')(app, requireAuth);
-  // Display and create campaigns
-  require('./routes/campaign')(app, requireAuth);
-  // Get OpenNorth Represent data
-  require('./routes/representatives')(app);
-  // Twilio app request URL
-  require('./routes/twiml')(app);
+// Homepage
+require('./routes/index')(app);
+// User authentication
+require('./routes/login')(app);
+// User creation
+require('./routes/user')(app);
+// User dashboard
+require('./routes/dashboard')(app, requireAuth);
+// Display and create campaigns
+require('./routes/campaign')(app, requireAuth);
+// Get OpenNorth Represent data
+require('./routes/representatives')(app);
+// Twilio app request URL
+require('./routes/twiml')(app);
 
-  // Bind the Express server to a port.
-  var port = app.settings.port;
-  app.listen(port, function() {
-    console.log("Listening on " + port);
-  });
-}());
+// Bind the Express server to a port.
+var port = app.settings.port;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
