a00a96b89eadb718db9ab3bceffb8a34fee67859
Shawn Price <shawnp@gmail.com>
2012-09-19 16:49:01 -0700

    Remove example error routes

--- routes/error.js
+++ routes/error.js
@@ -38,25 +38,4 @@ module.exports = function(app) {
       message: err
     });
   });
-
-  
-  app.get('/404', function(req, res, next){
-    // trigger a 404 since no other middleware
-    // will match /404 after this one, and we're not
-    // responding here
-    next();
-  });
-
-  app.get('/403', function(req, res, next){
-    // trigger a 403 error
-    var err = new Error('not allowed!');
-    err.status = 403;
-    next(err);
-  });
-
-  app.get('/500', function(req, res, next){
-    // trigger a generic (500) error
-    next(new Error('keyboard cat!'));
-  });
-  
 };
