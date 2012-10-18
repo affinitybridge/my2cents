9b5293c3b78b77f7aa26087cc7234b04b4fd500d
Shawn Price <shawnp@gmail.com>
2012-10-18 15:57:35 -0700

    Update to represent@0.2.0

--- routes/representatives.js
+++ routes/representatives.js
@@ -6,21 +6,24 @@ module.exports = function (app) {
   // A campaign will make an ajax to this route after determining lat/lon using
   // HTML Geolocation API.
   app.get('/representatives/:lat/:lon', function (req, res) {
-    represent.representativesLatLon(req.params.lat, req.params.lon, function (reps) {
-      if (reps && reps.objects.length) {
+    represent.representativesLatLon(req.params.lat, req.params.lon, function (error, data) {
+      if (error) {
+        throw error;
+      }
+      if (data && data.objects.length) {
         // Create an array of structured representative objects.
-        var processedReps = representatives.getProcessed(reps);
+        var processedReps = representatives.getProcessed(data);
         res.render('representatives', {
           layout: false,
           reps: processedReps
         });
       }
       // Open North Represent API not available.
-      else if (null === reps) {
+      else if (null === data) {
         res.send('The representatives database is unreachable at the moment. Please try again later.');
       }
       // No representatives found.
-      else if (0 === reps.objects.length) {
+      else if (0 === data.objects.length) {
         res.send('No representatives found for your current location.');
       }
     });
